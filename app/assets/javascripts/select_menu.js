55fa8ad2bb16c8314f86f2129be7dbd847231366
Ali Skinner <awskinner26@gmail.com>
2015-03-12 20:24:44 +0000

    adding other country images and js

--- app/assets/javascripts/select_menu.js
+++ app/assets/javascripts/select_menu.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
         if (country.name === chosenCountry) {
           $('<h2>' + country.name + '</h2>').prependTo('#other-header')
           $('<div class="stat">' + '<h4>Meal</h4>' + '<img src="assets/plate7.png" class="icon" id="other-meal">' + '<p id="other-meal-amount">' + country.meal + '</p>' + '</div>' +
-            '<div class="stat">' + '<h4>McDonalds</h4>' + '<p>' + country.mcdonalds + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>McDonalds</h4>' + '<img src="assets/fast-food.png" class="icon" id="other-mcdonalds">' + '<p id="other-mcdonalds-amount">' + country.mcdonalds + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Beer</h4>' + '<p>' + country.beer + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Coca-Cola</h4>' + '<p>' + country.coke + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Cigarettes</h4>' + '<p>' + country.cigs + '</p>' + '</div>' + 
@@ -38,11 +38,18 @@ $(document).ready(function() {
             '<div class="stat">' + '<h4>Shoes</h4>' + '<p>' + country.shoes + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Taxi (1km)</h4>' + '<p>' + country.taxi + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Coffee</h4>' + '<p>' + country.coffee + '</p>' + '</div>').appendTo('.other-all-stats')
-          ukMeal = parseFloat($('#uk-meal-amount').text());
-          otherMeal = parseFloat($('#other-meal-amount').text());
-          difference = otherMeal / ukMeal;
-          newSize = 250 * difference;
+
+          var ukMeal = parseFloat($('#uk-meal-amount').text());
+          var otherMeal = parseFloat($('#other-meal-amount').text());
+          var difference = otherMeal / ukMeal;
+          var newSize = 250 * difference;
           $('#other-meal').css({'width' : newSize, 'height' : newSize});
+
+          var ukMcdonalds = parseFloat($('#uk-mcdonalds-amount').text());
+          var otherMcdonalds = parseFloat($('#other-mcdonalds-amount').text());
+          var difference = otherMcdonalds / ukMcdonalds;
+          var newMcdonaldsSize = 250 * difference;
+          $('#other-mcdonalds').css({'width' : newMcdonaldsSize, 'height' : newMcdonaldsSize});
           // console.log('match!');
         }
       })

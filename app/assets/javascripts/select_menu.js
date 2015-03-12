88c60e208e46b9e34eb5979ebb65d6732702caec
Ali Skinner <awskinner26@gmail.com>
2015-03-12 21:17:44 +0000

    added functionality on all categories

--- app/assets/javascripts/select_menu.js
+++ app/assets/javascripts/select_menu.js
@@ -29,15 +29,15 @@ $(document).ready(function() {
           $('<h2>' + country.name + '</h2>').prependTo('#other-header')
           $('<div class="stat">' + '<h4>Meal</h4>' + '<img src="assets/plate7.png" class="icon" id="other-meal">' + '<p id="other-meal-amount">' + country.meal + '</p>' + '</div>' +
             '<div class="stat">' + '<h4>McDonalds</h4>' + '<img src="assets/fast-food.png" class="icon" id="other-mcdonalds">' + '<p id="other-mcdonalds-amount">' + country.mcdonalds + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Beer</h4>' + '<p>' + country.beer + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Coca-Cola</h4>' + '<p>' + country.coke + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Cigarettes</h4>' + '<p>' + country.cigs + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>1 Bed Apartment</h4>' + '<p>' + country.rent + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Cinema</h4>' + '<p>' + country.cinema + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Jeans</h4>' + '<p>' + country.jeans + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Shoes</h4>' + '<p>' + country.shoes + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Taxi (1km)</h4>' + '<p>' + country.taxi + '</p>' + '</div>' + 
-            '<div class="stat">' + '<h4>Coffee</h4>' + '<p>' + country.coffee + '</p>' + '</div>').appendTo('.other-all-stats')
+            '<div class="stat">' + '<h4>Beer</h4>' + '<img src="assets/beer34.png" class="icon" id="other-beer">' + '<p id="other-beer-amount">' + country.beer + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>Coca-Cola</h4>' + '<img src="assets/coke.png" class="icon" id="other-coke">' + '<p id="other-coke-amount">' + country.coke + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>Cigarettes</h4>' + '<img src="assets/cigarette8.png" class="icon" id="other-cigs">' + '<p id="other-cigs-amount">' + country.cigs + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>1 Bed Apartment</h4>' + '<img src="assets/house121.png" class="icon" id="other-rent">' + '<p id= "other-rent-amount">' + country.rent + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>Cinema</h4>' + '<img src="assets/clapperboard2.png" class="icon" id="other-cinema">' + '<p id="other-cinema-amount">' + country.cinema + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>Jeans</h4>' + '<img src="assets/jeans.png" class="icon" id="other-jeans">' + '<p id="other-jeans-amount">' + country.jeans + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>Shoes</h4>' + '<img src="assets/trail.png" class="icon" id="other-shoes">' + '<p id="other-shoes-amount">' + country.shoes + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>Taxi (1km)</h4>' + '<img src="assets/taxi14.png" class="icon" id="other-taxi">' + '<p id="other-taxi-amount">' + country.taxi + '</p>' + '</div>' + 
+            '<div class="stat">' + '<h4>Coffee</h4>' + '<img src="assets/hot51.png" class="icon" id="other-coffee">' + '<p id="other-coffee-amount">' + country.coffee + '</p>' + '</div>').appendTo('.other-all-stats')
 
           var ukMeal = parseFloat($('#uk-meal-amount').text());
           var otherMeal = parseFloat($('#other-meal-amount').text());
@@ -51,6 +51,66 @@ $(document).ready(function() {
           var newMcdonaldsSize = 250 * difference;
           $('#other-mcdonalds').css({'width' : newMcdonaldsSize, 'height' : newMcdonaldsSize});
           // console.log('match!');
+
+          var ukBeer = parseFloat($('#uk-beer-amount').text());
+          var otherBeer = parseFloat($('#other-beer-amount').text());
+          var difference = otherBeer / ukBeer;
+          var newBeerSize = 250 * difference;
+          $('#other-beer').css({'width' : newBeerSize, 'height' : newBeerSize});
+
+          var ukCoke = parseFloat($('#uk-coke-amount').text());
+          var otherCoke = parseFloat($('#other-coke-amount').text());
+          var difference = otherCoke / ukCoke;
+          var newCokeSize = 250 * difference;
+          $('#other-coke').css({'width' : newCokeSize, 'height' : newCokeSize});
+
+          var ukCigarette = parseFloat($('#uk-cigs-amount').text());
+          var otherCigarette = parseFloat($('#other-cigs-amount').text());
+          var difference = otherCigarette / ukCigarette;
+          var newCigaretteSize = 250 * difference;
+          $('#other-cigs').css({'width' : newCigaretteSize, 'height' : newCigaretteSize});
+
+          var ukRent = parseFloat($('#uk-rent-amount').text());
+          var otherRent = parseFloat($('#other-rent-amount').text());
+          var difference = otherRent / ukRent;
+          var newRentSize = 250 * difference;
+          $('#other-rent').css({'width' : newRentSize, 'height' : newRentSize});
+
+          var ukCinema = parseFloat($('#uk-cinema-amount').text());
+          var otherCinema = parseFloat($('#other-cinema-amount').text());
+          var difference = otherCinema / ukCinema;
+          var newCinemaSize = 250 * difference;
+          $('#other-cinema').css({'width' : newCinemaSize, 'height' : newCinemaSize});
+
+          var ukJeans = parseFloat($('#uk-jeans-amount').text());
+          var otherJeans = parseFloat($('#other-jeans-amount').text());
+          var difference = otherJeans / ukJeans;
+          var newJeansSize = 250 * difference;
+          $('#other-jeans').css({'width' : newJeansSize, 'height' : newJeansSize});
+
+          var ukJeans = parseFloat($('#uk-jeans-amount').text());
+          var otherJeans = parseFloat($('#other-jeans-amount').text());
+          var difference = otherJeans / ukJeans;
+          var newJeansSize = 250 * difference;
+          $('#other-jeans').css({'width' : newJeansSize, 'height' : newJeansSize});
+
+          var ukShoes = parseFloat($('#uk-shoes-amount').text());
+          var otherShoes = parseFloat($('#other-shoes-amount').text());
+          var difference = otherShoes / ukShoes;
+          var newShoesSize = 250 * difference;
+          $('#other-shoes').css({'width' : newShoesSize, 'height' : newShoesSize});
+
+          var ukTaxi = parseFloat($('#uk-taxi-amount').text());
+          var otherTaxi = parseFloat($('#other-taxi-amount').text());
+          var difference = otherTaxi / ukTaxi;
+          var newTaxiSize = 250 * difference;
+          $('#other-taxi').css({'width' : newTaxiSize, 'height' : newTaxiSize});
+
+          var ukCoffee = parseFloat($('#uk-coffee-amount').text());
+          var otherCoffee = parseFloat($('#other-coffee-amount').text());
+          var difference = otherCoffee / ukCoffee;
+          var newCoffeeSize = 250 * difference;
+          $('#other-coffee').css({'width' : newCoffeeSize, 'height' : newCoffeeSize});
         }
       })
     })

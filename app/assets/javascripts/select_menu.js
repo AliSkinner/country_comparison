4921569534dab8e717958aad8e358e13b9a80db0
Mathew Hawley <mathew@mathewhawley.com>
2015-03-12 20:04:20 +0000

    Resize other country icons based on difference with UK

--- app/assets/javascripts/select_menu.js
+++ app/assets/javascripts/select_menu.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
         // console.log(country.meal);
         if (country.name === chosenCountry) {
           $('<h2>' + country.name + '</h2>').prependTo('#other-header')
-          $('<div class="stat">' + '<h4>Meal</h4>' + '<img src="assets/plate7.png" class="icon" id="other-meal">' + '<p>' + country.meal + '</p>' + '</div>' +
+          $('<div class="stat">' + '<h4>Meal</h4>' + '<img src="assets/plate7.png" class="icon" id="other-meal">' + '<p id="other-meal-amount">' + country.meal + '</p>' + '</div>' +
             '<div class="stat">' + '<h4>McDonalds</h4>' + '<p>' + country.mcdonalds + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Beer</h4>' + '<p>' + country.beer + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Coca-Cola</h4>' + '<p>' + country.coke + '</p>' + '</div>' + 
@@ -38,7 +38,12 @@ $(document).ready(function() {
             '<div class="stat">' + '<h4>Shoes</h4>' + '<p>' + country.shoes + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Taxi (1km)</h4>' + '<p>' + country.taxi + '</p>' + '</div>' + 
             '<div class="stat">' + '<h4>Coffee</h4>' + '<p>' + country.coffee + '</p>' + '</div>').appendTo('.other-all-stats')
-          // console.log('match!');  
+          ukMeal = parseFloat($('#uk-meal-amount').text());
+          otherMeal = parseFloat($('#other-meal-amount').text());
+          difference = otherMeal / ukMeal;
+          newSize = 250 * difference;
+          $('#other-meal').css({'width' : newSize, 'height' : newSize});
+          // console.log('match!');
         }
       })
     })

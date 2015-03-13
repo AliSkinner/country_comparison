63065944dd9a7dac1aaaac76914d059508f21edf
Mathew Hawley <mathew@mathewhawley.com>
2015-03-13 08:35:18 +0000

    Fix div height

--- app/assets/javascripts/custom_waypoints.js
+++ app/assets/javascripts/custom_waypoints.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
   var waypoint = new Waypoint({
     element: document.getElementById('uk-cigs'),
     handler: function(direction) {
-      console.log('waypoing')
-      notify(this.id + ' hit')
+      console.log(direction)
     }
   })
 
